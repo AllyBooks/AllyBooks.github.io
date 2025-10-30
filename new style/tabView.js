67f95d3e19c47f734a64daafc1ed2a8f7483fc93
AllyBooks <allybooks.comics@gmail.com>
2025-10-30 14:54:08 +0000

    add made by and change some animation

--- new style/tabView.js
+++ new style/tabView.js
@@ -3,6 +3,8 @@ document.querySelector('#setting-btn').addEventListener('click', () => {
 });
 
 const btn = document.getElementById('setting-btn');
+const btn2 = document.getElementById('made_by');
 btn.addEventListener('click', () => {
   btn.classList.toggle('active');
+  btn2.classList.toggle('active');
 });
--- tabView.js
+++ tabView.js
@@ -1,3 +1,10 @@
 document.querySelector('#setting-btn').addEventListener('click', () => {
-    document.querySelector('#animation').classList.toggle('open');
-  });
+  document.querySelector('#animation').classList.toggle('open');
+});
+
+const btn = document.getElementById('setting-btn');
+const btn2 = document.getElementById('made_by');
+btn.addEventListener('click', () => {
+  btn.classList.toggle('active');
+  btn2.classList.toggle('active');
+});
