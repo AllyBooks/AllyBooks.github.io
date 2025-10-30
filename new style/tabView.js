375ddfd76ae5517bb0976f69b4fa8bef9a9b7be2
AllyBooks <allybooks.comics@gmail.com>
2025-10-30 13:35:03 +0000

    add new burger menu icon and add animation

--- new style/tabView.js
+++ new style/tabView.js
@@ -1,3 +1,8 @@
 document.querySelector('#setting-btn').addEventListener('click', () => {
-    document.querySelector('#animation').classList.toggle('open');
-  });
+  document.querySelector('#animation').classList.toggle('open');
+});
+
+const btn = document.getElementById('setting-btn');
+btn.addEventListener('click', () => {
+  btn.classList.toggle('active');
+});
