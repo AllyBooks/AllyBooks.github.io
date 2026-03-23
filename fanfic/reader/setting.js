4d9ad9f1b0fe88de7bc0765725452766a1e711c8
Vadosmik <wadimmikanowicz@gmail.com>
2026-03-23 19:18:51 +0100

    few changes in menu(textSize)

--- fanfic/reader/setting.js
+++ fanfic/reader/setting.js
@@ -1,5 +1,8 @@
 const menu = document.getElementById('myMenu');
+const textSizeValue = document.getElementById('text-size-value');
 const slider = document.getElementById('textSize');
+
+document.documentElement.style.setProperty('--font-size-base', slider.value + 'px');
 
 function openSetting() {
   menu.hidden = !menu.hidden;
@@ -7,6 +10,8 @@ function openSetting() {
 
 slider.addEventListener('input', (event) => {
   const newSize = event.target.value + 'px';
+
+  textSizeValue.textContent = newSize;
   
   document.documentElement.style.setProperty('--font-size-base', newSize);
 });
